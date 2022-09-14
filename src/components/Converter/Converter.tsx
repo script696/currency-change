28a1551e16e07c6ff86d3d4385ec55da5d86bb77
Nikita <script696@yandex.ru>
2022-09-14 16:32:17 +0300

    feat: Project start
    initial commit

---
+++ src/components/Converter/Converter.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Converter.module.scss'
+
+
+
+const Converter = () => {
+  return (
+    <section className={s.converter}>
+
+    </section>
+  )
+}
+
+export default Converter
